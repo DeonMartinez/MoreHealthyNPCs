f6feead1c513cea5ef21a981f1bffb0e1ddccc1c
DeonMartinez <49213704+DeonMartinez@users.noreply.github.com>
2020-11-09 23:44:47 -0700

    Add files via upload

---
+++ Assets/Scripts/KnightHelth.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KnightHelth : MonoBehaviour, IHealth
+{
+    [SerializeField]
+    private int startingHealth = 100;
+
+    private int currentHealth;
+
+    public event Action<float> OnHPPctChanged = delegate { };
+    public event Action OnDied = delegate { };
+
+    public int count = 0;
+
+    private void Start()
+    {
+        currentHealth = startingHealth;
+    }
+
+    public float CurrentHpPct
+    {
+        get { return (float)currentHealth / (float)startingHealth; }
+    }
+
+    public void TakeDamage(int amount)
+    {
+        if (amount <= 0)
+        {
+            throw new ArgumentOutOfRangeException("How do you take negative damage? ... are you trying to heal them?" + amount);
+
+        }
+
+        count++;
+        if(count == 5)
+        {
+            currentHealth -= amount;
+
+            OnHPPctChanged(CurrentHpPct);
+            count = 0;
+        }
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        OnDied();
+        GameObject.Destroy(this.gameObject);
+    }
+}
---
+++ Assets/Scripts/RoyalHealth.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoyalHealth : MonoBehaviour, IHealth
+{
+    [SerializeField]
+    private int startingHealth = 200;
+
+    private int currentHealth;
+
+    public event Action<float> OnHPPctChanged = delegate { };
+    public event Action OnDied = delegate { };
+
+    public bool bleeding = false;
+
+    private void Start()
+    {
+        currentHealth = startingHealth;
+    }
+
+    public float CurrentHpPct
+    {
+        get { return (float)currentHealth / (float)startingHealth; }
+    }
+
+    public void TakeDamage(int amount)
+    {
+        if (amount <= 0)
+        {
+            throw new ArgumentOutOfRangeException("How do you take negative damage? ... are you trying to heal them?" + amount);
+        }
+
+        currentHealth -= amount;
+
+        OnHPPctChanged(CurrentHpPct);
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+
+        //bleeding = true;   
+    }
+
+    public void BleedOut()
+    {
+
+            for (int i = 1; i < 20; i++)
+            {
+
+                TakeDamage(1);
+            }
+        
+    }
+
+    public void Update()
+    {
+        if (bleeding == true)
+        {
+            BleedOut();
+        }
+    }
+
+    private void Die()
+    {
+        OnDied();
+        GameObject.Destroy(this.gameObject);
+    }
+}
